d42fd61779fb383addcfaac3f2b42396afbd8462
糊涂涂 <103564619+TLD1027@users.noreply.github.com>
2023-08-05 21:24:18 +0800

    Update t1dpwn.py

--- t1dpwn.py
+++ t1dpwn.py
@@ -8,23 +8,27 @@ t_lib = null
 t_arch = null
 t_patch = null
 t_path = "/home/t1d/tools"
+t_pwnlib = null
+t_pwnfile = null
 
 def tset(my_file):
     global t_file, tlib, tarch, t_patch
     t_file = my_file
     t_lib = tlibc()
     t_arch = tarch()
-    if t_lib == null or t_arch == null:
-        log.error(f"error!")
-    else:
+    if t_lib and t_arch:
         t_patch = f"{t_lib}_{t_arch}"
+        
+def tbegin():
+    global t_pwnfile, t_pwnlib
+    t_pwnfile = ELF(f"{t_file}")
+    t_pwnlib = t_pwnfile.libc
         
 def search_folder(root_folder, target_folder):
     target_path = os.path.join(root_folder, target_folder)
     return os.path.exists(target_path)
             
 def find_so_files(directory):
-    # 在给定目录下查找所有 .so 和 .so.6 文件
     so_files = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -44,7 +48,7 @@ def execute_command(command):
         stderr_output = result.stderr.strip()
         return stdout_output, stderr_output
     except subprocess.CalledProcessError as e:
-        log.error(f"命令执行出错: {e}")
+        log.error(f"ERROR: {e}")
         return None, None
 
 def tlibc():
@@ -59,18 +63,18 @@ def tlibc():
                 version_match = re.search(r'(\d+\.\d+\-\S+)(?=\))', stdout)
                 if version_match:
                     version_info = version_match.group()
-                    log.success(f"版本号信息: {version_info}")
+                    log.success(f"Glibc version: {version_info}")
                     return version_info
                 else:
-                    log.error(f"未找到文件 {so_file} 的版本号信息。")
+                    log.warning(f"Version information for the file {so_file} was not found.")
                     return null
     else:
-        log.error("未找到 .so 或 .so.6 文件")
+        log.warning("The .so or .so.6 file was not found.")
         return null
         
 def tarch():
     if t_file == null:
-        log.error("请先设置文件！")
+        log.error("Please configure the file first!")
         return null
     command = f"checksec {t_file}"
     stdout, stderr = execute_command(command)
@@ -78,28 +82,28 @@ def tarch():
         arch_match = re.search(r'Arch:\s+(amd64|i386)', stderr)
         if arch_match:
             arch_info = arch_match.group(1)
-            log.success(f"架构信息: {arch_info}")
+            log.success(f"Arch: {arch_info}")
             return arch_info
         else:
-            log.error("未找到架构信息。")
+            log.error("Architecture information not found.")
             return null
     else:
-        log.error("未找到文件")
+        log.error("The file was not found.")
         return null
     
 def tpatchelf(*args):
     if t_patch == null:
-        log.error(f"tset at first!")
+        log.warning(f"tset at first!")
         return
     if os.path.exists(f"{t_path}/glibc-all-in-one/libs/{t_patch}"):
         pass
     else:
-        log.error(f"请先自行下载对应版本的glibc文件包!")
+        log.warning(f"Please download the corresponding version of the glibc package first!")
         return
     command = f"patchelf --replace-needed libc.so.6 {t_path}/glibc-all-in-one/libs/{t_patch}/libc.so.6 {t_file}"
     stdout, stderr = execute_command(command)
     if stderr:
-        log.error(f"patchelf libc.so.6文件失败！")
+        log.error(f"Patchelf failed for libc.so.6 file!")
         return
     if "amd64" in t_patch:
         command = f"patchelf --set-interpreter {t_path}/glibc-all-in-one/libs/{t_patch}/ld-linux-x86-64.so.2 {t_file}"
@@ -108,13 +112,13 @@ def tpatchelf(*args):
         command = f"patchelf --set-interpreter {t_path}/glibc-all-in-one/libs/{t_patch}/ld-linux.so.2 {t_file}"
         stdout, stderr = execute_command(command)
     if stderr:
-        log.error(f"patchelf *.so.2文件失败！")
+        log.error(f"Patchelf failed for *.so.2 file!")
         return   
     if args:
         for arg in args:
             command = f"patchelf --add-needed ./{arg} {t_file}"
             stdout, stderr = execute_command(command)
             if stderr:
-                log.error(f"patchelf libc.so.6文件失败！")
+                log.error(f"patchelf failed for {arg} file!")
                 return
-    log.success(f"patchelf成功!")
+    log.success(f"Patchelf successfully!")
